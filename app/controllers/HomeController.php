416183d1b1e1746b542786258359cda709add7eb
kcates81 <kriscates81@gmail.com>
2016-06-01 15:40:00 -0500

    added logic from routes.php

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -20,4 +20,43 @@ class HomeController extends BaseController {
 		return View::make('hello');
 	}
 
+	public function showResume()
+	{
+		$name = 'something';
+
+	    $data = array(
+	        'name' => $name
+	    );
+
+	    return View::make('resume')->with($data);
+	}
+
+	public function showPortfolio()
+	{
+		$name = 'something else';
+
+	    $data = array(
+	        'name' => $name
+	    );
+
+	    return View::make('portfolio')->with($data);
+	}
+
+	public function rollDice($guess)
+	{
+		$min = 1;
+	    $max = 6;
+	    $message = '';
+	    $randomNum = mt_rand((int)$min, (int)$max);
+
+	    $message = ($guess == $randomNum) ? 'You guessed it!' : 'Nope!';
+	    
+	    $data = array(
+	        'randomNum' => $randomNum,
+	        'guess' => $guess,
+	        'message' => $message
+	    );
+
+	    return View::make('roll-dice')->with($data);
+	}
 }
