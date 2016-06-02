8aeb873f0bb98d0ee0fb646a082079ac5fc66920
kcates81 <kriscates81@gmail.com>
2016-06-02 18:13:24 -0500

    made a bunch of changes to css. made links to pages

--- app/routes.php
+++ app/routes.php
@@ -20,3 +20,20 @@ Route::get('/portfolio', 'HomeController@showPortfolio');
 Route::get('/rolldice/{guess?}', 'HomeController@rollDice');
 
 Route::resource('/posts', 'PostsController');
+
+Route::get('orm-test', function ()
+{
+    $post1 = new Post();
+    $post1->user_id = 1;
+    $post1->title = 'Eloquent is awesome!';
+    $post1->description = 'Look at me describing things';
+    $post1->body  = 'It is super easy to create a new post.';
+    $post1->save();
+
+    $post2 = new Post();
+    $post2->user_id = 1;
+    $post2->title = 'Post number two';
+    $post2->description = 'Description for number two';
+    $post2->body  = 'The body for post number two.';
+    $post2->save();
+});
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,15 +2,63 @@
 <html lang="en">
 <head>
     <title>Laravel Blog</title>
+    <style type="text/css">
+        .btn {
+           background-color: #FF8A00 !important; 
+        }
+
+        h1 {
+            font-weight: 200px;
+        }
+        h3 {
+            color: #FF8A00;
+        }
+
+        .input-field input[type=text]:focus {
+            border-bottom: 1px solid #03a9f4 !important;
+            box-shadow: 0 1px 0 0 #03a9f4 !important;
+       }
+
+        .input-field label {
+            color: #03a9f4 !important;
+        }
+            
+        nav {
+            background-color: #03a9f4 !important;
+            height: 150px !important; 
+        }
+    </style>
     <!-- Compiled and minified CSS -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+     <!-- Materialize Icons -->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#!" class="brand-logo"><img src="/img/mylogo_white.png"></a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li class="active"><a href="{{action('PostsController@index')}}">Posts</a></li>
+                <li class="active"><a href="{{action('PostsController@create')}}">Create a post?</a></li>
+            </ul>
+            <ul class="side-nav" id="mobile-demo">
+                <li class="active"><a href="{{action('PostsController@index')}}"">Posts</a></li>
+                <li class="active"><a href="{{action('PostsController@create')}}"">Create a post</a></li>
+            </ul>
+        </div>
+    </nav>
+
     @yield('content')
 
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+    <script>
+        $( document ).ready(function(){
+            $(".button-collapse").sideNav();
+        })
+    </script>
 </body>
 </html>
