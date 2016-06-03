9d3ccfbc28f6bdb5cae4c96d10ecf3ffa0ffabce
kcates81 <kriscates81@gmail.com>
2016-06-03 15:54:56 -0500

    added some changes to my jQuery and made the logo clickable

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
 <body>
     <nav>
         <div class="nav-wrapper">
-            <a href="#!" class="brand-logo"><img src="/img/mylogo_white.png"></a>
+            <a href="{{action('PostsController@index')}}" class="brand-logo"><img src="/img/mylogo_white.png"></a>
             <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
             <ul class="right hide-on-med-and-down">
                 <li class="active"><a href="{{action('PostsController@index')}}">Posts</a></li>
@@ -36,5 +36,6 @@
             $(".button-collapse").sideNav();
         })
     </script>
+    @yield('bottom-script')
 </body>
 </html>
