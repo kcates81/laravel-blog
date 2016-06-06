d59ec04f707500cd48ebfdae5066219c7a7038e1
kcates81 <kriscates81@gmail.com>
2016-06-06 17:49:13 -0500

    added template content to the resume page

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>{{{$name}}}</h1>
-    <a href="{{{ action('HomeController@showPortfolio')}}}">Portfolio</a>
+    <ul class="collapsible" data-collapsible="accordion">
+        <li>
+            <div class="collapsible-header"><i class="material-icons">filter_drama</i>Summary Statement</div>
+            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+        </li>
+        <li>
+            <div class="collapsible-header"><i class="material-icons">place</i>Skills</div>
+            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+        </li>
+        <li>
+            <div class="collapsible-header"><i class="material-icons">whatshot</i>Projects</div>
+            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+        </li>
+        <li>
+            <div class="collapsible-header"><i class="material-icons">whatshot</i>Education and Certifications</div>
+            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+        </li>
+        <li>
+            <div class="collapsible-header"><i class="material-icons">whatshot</i>Previous Work Experience</div>
+            <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+        </li>
+    </ul>
+        
+@stop
+
+@section('bottom-script')
+    <script>
+        $(document).ready(function(){
+            $('.collapsible').collapsible({
+                accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+            });
+        });
+    </script>
 @stop
