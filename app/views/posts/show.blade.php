618d05b10e67a6ce94d99a7ee38a9c16199a6692
kcates81 <kriscates81@gmail.com>
2016-06-06 11:21:51 -0500

    added #timestamp id to my timestamps so that I could get the font color to change. This works on my /index page, but not on my /show page

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <h1 class="center-align">{{{ $post->title }}}</h1> 
         <h3>{{{ $post->description }}}</h3>
-        <p>Posted On: {{{ $post->created_at->setTimezone('America/Chicago')->format('D, M, j, Y @ h:i:s A') }}} | Last Modified On: {{{ $post->updated_at->setTimezone('America/Chicago')->format('D, M, j, Y @ h:i:s A') }}}</p>
+        <p id="timestap">Posted On: {{{ $post->created_at->setTimezone('America/Chicago')->format('D, M, j, Y @ h:i:s A') }}} | Last Modified On: {{{ $post->updated_at->setTimezone('America/Chicago')->format('D, M, j, Y @ h:i:s A') }}}</p>
         <p class="flow-text">{{{ $post->body }}}</p> 
         <button class="btn"><a href="{{{ action('PostsController@edit', $post->id) }}}">Edit Your Post</a></button>
     </div> 
