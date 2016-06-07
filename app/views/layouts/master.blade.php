0d1eb5714c361b6c1e74fcfac3f12fd443eb2898
kcates81 <kriscates81@gmail.com>
2016-06-07 08:52:17 -0500

    updated the footer to contain my contact information

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -46,12 +46,29 @@
     @yield('content')
 
     <footer class="page-footer">
-        <div class="footer-copyright">
-            <div class="container">
-                © 2016 Kristen L. Cates, PMP
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact Me</h5>
+                <p class="grey-text text-lighten-4"><i class="fa fa-map-marker" aria-hidden="true"></i>11935 Travis Path<br>San Antonio, TX 78253<br><i class="fa fa-mobile" aria-hidden="true"></i>210.278.8717<br><a class="grey-text text-lighten-3" href="#!"><i class="fa fa-envelope" aria-hidden="true"></i>kriscates81@gmail.com</a></p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Online Presence</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="https://github.com/kcates81" target="_blank"><i class="fa fa-github" aria-hidden="true"></i>kcates81</a></li>
+                    <li><a class="grey-text text-lighten-3" href="https://twitter.com/kriscates81" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i>@kriscates81</a></li>
+                    <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/kristenladonacates" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i>kristenladonacates</a></li>
+                </ul>
+              </div>
             </div>
-        </div>
-    </footer>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2016 Kristen L. Cates, PMP
+            <a class="grey-text text-lighten-4 right" href="#!">About this Site</a>
+            </div>
+          </div>
+        </footer>
 
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
