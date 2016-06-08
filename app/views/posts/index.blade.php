cdf424f753330d121220c142d24f1ab0bb4781c0
kcates81 <kriscates81@gmail.com>
2016-06-08 08:34:46 -0500

    added image upload functionality

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -9,7 +9,7 @@
                 <h2><a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></h2>
                 <h3>{{{ $post->description }}}</h3>
                 <p id="timestamp">Posted On: {{{ $post->created_at->setTimezone('America/Chicago')->format('D, M, j, Y @ h:i:s A') }}} | Last Modified On: {{{ $post->updated_at->setTimezone('America/Chicago')->format('D, M, j, Y @ h:i:s A') }}} | Written by: {{{ $post->user->username }}}</p>
-                <p class="flow-text truncate">{{{ $post->body }}}</p>
+                <p class="flow-text truncate">{{ $post->body }}</p>
                 <div class="divider"></div>
             @endforeach  
         </div> 
@@ -20,4 +20,9 @@
        
     </div>
     
+@stop
+
+@section('bottom-script')
+
+
 @stop
