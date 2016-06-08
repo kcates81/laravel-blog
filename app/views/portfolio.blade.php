0594df8e372e64aba07e63a8635b9e128c05f9c8
kcates81 <kriscates81@gmail.com>
2016-06-08 11:57:26 -0500

    updated my portfolio page with images of the projects I want to highlight

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -4,31 +4,31 @@
     <div class="slider">
         <ul class="slides">
             <li>
-                <img src="http://lorempixel.com/580/250/nature/1"> <!-- random image -->
+                <img src="/img/screenshots/adlister.png">
                 <div class="caption center-align">
-                    <h3>This is our big Tagline!</h3>
-                    <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    <h3>Adlister Project</h3>
+                    <h5 class="light grey-text text-lighten-3">Checkout the Code on GitHub</h5>
             </div>
             </li>
             <li>
-                <img src="http://lorempixel.com/580/250/nature/2"> <!-- random image -->
+                <img src="/img/screenshots/national-parks.png">
                 <div class="caption left-align">
-                    <h3>Left Aligned Caption</h3>
-                    <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    <h3>National Parks Project</h3>
+                    <h5 class="light grey-text text-lighten-3">Checkout the Code on GitHub</h5>
                 </div>
             </li>
             <li>
-                <img src="http://lorempixel.com/580/250/nature/3"> <!-- random image -->
+                <img src="/img/screenshots/weather.png"> <!-- random image -->
                 <div class="caption right-align">
                     <h3>Right Aligned Caption</h3>
-                    <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    <h5 class="light grey-text text-lighten-3">Checkout the Code on GitHub</h5>
                 </div>
             </li>
             <li>
                 <img src="http://lorempixel.com/580/250/nature/4"> <!-- random image -->
                 <div class="caption center-align">
                     <h3>This is our big Tagline!</h3>
-                    <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    <h5 class="light grey-text text-lighten-3">Checkout the Code on GitHub</h5>
             </div>
             </li>
         </ul>
