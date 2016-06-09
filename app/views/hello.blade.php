a286d1b2a40e4d01a5c9cc8748272381e7a7f026
kcates81 <kriscates81@gmail.com>
2016-06-09 13:32:13 -0500

    changed the alignment of the image and the text on the home page back to how it originally was. it wasn't responsive on smaller screens

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -6,14 +6,10 @@
     </div>
     <div class="section white">
         <div class="row container">
-            <div class="col s2">
                 <img src="/img/headshot.jpeg">   
-            </div>
-            <div class="col s10">
                 <h1 class="header">Kristen L. Cates, PMP</h1>
                 <h3>Full-Stack Web Developer</h3>
                 <p class="grey-text text-darken-3 lighten-3">Entry-level LAMP+J Developer transitioning from a 10+ year career in proposal/project management and business development. </p>   
-            </div>
         </div>
     </div>
     <div class="parallax-container">
