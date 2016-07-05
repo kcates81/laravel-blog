a1e764dde648922511ce1364232b746386b176ef
kcates81 <kriscates81@gmail.com>
2016-07-05 14:03:38 -0500

    added capstone project to portfolio carousel

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -8,21 +8,28 @@
                 <a href="https://github.com/greasy-blanket/adlister.dev" target="_blank"><img class="responsive-img" src="/img/screenshots/adlister.png"></a>
                 <div class="caption left-align">
                     <h3><strong>Adlister Project</strong></h3>
-                    <h5>Built using PHP, MySQL, HTML, Google's Materialize, and Custom CSS</h5>
+                    <h5>Built using PHP, MySQL, HTML, Materialize CSS, and Custom CSS</h5>
                 </div>
             </li>
             <li>
                 <a href="https://github.com/kcates81/Codeup-Web-Exercises/blob/master/public/national_parks.php" target="_blank"><img class="responsive-img" src="/img/screenshots/national-parks.png"></a>
                 <div class="caption right-align">
                     <h3><strong>National Parks Project</strong></h3>
-                    <h5>Built using PHP, MySQL, HTML, Google's Materialize, and Custom CSS</h5>
+                    <h5>Built using PHP, MySQL, HTML,Materialize CSS, and Custom CSS</h5>
                 </div>
             </li>
             <li>
                 <a href="https://github.com/kcates81/Codeup-Web-Exercises/blob/master/public/weather_map.js" target="_blank"><img class="responsive-img" src="/img/screenshots/weather.png"></a>
                 <div class="caption right-align">
                     <h3><strong>Weather Map Project</strong></h3>
-                    <h5>Built using jQuery, HTML, Google's Materialize, Custom CSS, Google Map API, and Open Weather Map API</h5>
+                    <h5>Built using jQuery, HTML, Materialize CSS, Custom CSS, Google Map API, and Open Weather Map API</h5>
+                </div>
+            </li>
+            <li>
+                <a href="https://github.com/Freelancer-Invoice-Tool/flit.dev" target="_blank"><img class="responsive-img" src="/img/screenshots/flit.png"></a>
+                <div class="caption right-align">
+                    <h3><strong>Capstone Project</strong></h3> 
+                    <h5>Built using Laravel, MySQL, jQuery, HTML, Materialize CSS, Custom CSS, Mailgun API, and Stripe API</h5> 
                 </div>
             </li>
         </ul>
