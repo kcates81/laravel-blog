483e21c1bddcc608053363ebf070c812818388f6
kcates81 <kriscates81@gmail.com>
2016-07-11 15:32:39 -0500

    starting trying to mess with the grid layout. removed section class from the blog posts. i keep trying to use inline-block to get the twitter div and the post div to align next to one another, but I'm still unable to move the twitter feed out from under the blog posts

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <h1 class="center-align">Blog Posts</h1>  
-        <div id="post-box" class="section">
+        <div id="post-box" class="col s12 m8 l9 ">
             @foreach($posts as $post)
                 <h2><a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></h2>
                 <h3>{{{ $post->description }}}</h3>
@@ -15,16 +15,14 @@
                 <div class="divider"></div>
             @endforeach  
         </div> 
-        <div id="twitter-box">
+        <div id="twitter-box" class="col s12 m4 l3">
              <a class="twitter-timeline" data-width="500" data-height="1000" href="https://twitter.com/kriscates81">Tweets by kriscates81</a> 
         </div>
         
         <ul class="pagination center-align">
             {{ $paginator->render() }}
         </ul>
-       
-    </div>
-    
+    </div> 
 @stop
 
 @section('bottom-script')
