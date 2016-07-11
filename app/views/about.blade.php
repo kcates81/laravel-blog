f5802ad440fdea1f96a8ed81f22f8c3dae5d9cdf
kcates81 <kriscates81@gmail.com>
2016-07-11 15:38:59 -0500

    made changes to the about page text

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <h1 class="center-align">About this Site</h1>
-        <p class="flow-text">This site is one of my last individual projects during my time at Codeup. I built this from scratch using the Laravel 4.2 framework. The basis for CSS styling on the site is Google's Materialize CSS with custom CSS on top of it. I tried to use a variety of Materialize's features to familiarize myself with how to implement them on a working site. These features include responsive navigation bars, parallaxes, and sliders. I used MySQL (SequelPro) for all database functionality. I plan to continue to add and refine the functionality of this site as I learn more about Laravel and Materialize.</p>   
+        <p class="flow-text">This site is one of my last individual projects during my time at Codeup. I built this from scratch using the Laravel 4.2 framework. The basis for CSS styling on the site is Materialize CSS with custom CSS on top of it. I tried to use a variety of Materialize's features to familiarize myself with how to implement them on a working site. These features include responsive navigation bars, parallaxes, and sliders. I used MySQL (SequelPro) for all database functionality. I plan to continue to add and refine the functionality of this site as I learn more about Laravel and Materialize.</p>  
     </div>
 
 @stop
